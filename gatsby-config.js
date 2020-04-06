9af7c06cf6bed11c9afea43ab4e58291429a85c3
jgam <jgam@alumni.nd.edu>
2020-04-06 17:07:49 +0900

    atomic design start

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/components/atoms/buttons/loginButton/loginButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loginButton() {
+    return (
+        <div>
+            <Button>Login</Button>
+        </div>
+    )
+}
+
+export default loginButton
